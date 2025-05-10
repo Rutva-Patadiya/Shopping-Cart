8456197f4dd3c1c5944d92800d4053f25ef2bb25
Rutva Soni <patadiyarutva@gmail.com>
2025-05-10 16:37:18 +0530

    Error Resolved of xcode

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:shopping_cart/product/product_page.dart';
+import 'package:shopping_cart/signup/signup_page.dart';
 import 'package:shopping_cart/utils/router_utils.dart';
 
 import 'core/utils/theme/theme.dart';
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         theme: TAppTheme.lightTheme,
         onGenerateRoute: onGenerateRoutes,
         // Apply current locale
-        home: ProductList(),
+        home: SignupPage(),
       ),
     );
   }
--- lib/utils/router_utils.dart
+++ lib/utils/router_utils.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../home/home_page.dart';
 import '../login/login_page.dart';
+import '../product/product_page.dart';
 import '../signup/signup_page.dart';
 
 Route<dynamic> onGenerateRoutes(RouteSettings routeSettings) {
@@ -12,8 +13,8 @@ Route<dynamic> onGenerateRoutes(RouteSettings routeSettings) {
     case Login.route:
       return MaterialPageRoute(builder: (context) => Login());
 
-    // case ProductList.route:
-    //   return MaterialPageRoute(builder: (context) => ProductList());
+    case ProductList.route:
+      return MaterialPageRoute(builder: (context) => ProductList());
 
     // case Product.route:
     //   return MaterialPageRoute(builder: (context) => HomePage());
